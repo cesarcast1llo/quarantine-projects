88585f3be1f5879ff31ed4bd549ad8cea6f74171
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-05-13 11:31:44 -0700

    finalized node setup

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+// const router = express.Router();
 
 var port = process.env.PORT || 3000;
 
-router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/index.html'));
-});
+app.use(express.static(__dirname));
+app.use(express.static('pages'));
+app.use(express.static('assets'));
 
-// router.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/about.html'));
-// });
-
-// router.get('/sitemap', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/sitemap.html'));
-// });
-
-//add the router
-app.use('/', router);
 app.listen(port, function () {
   console.log(`running on port ${port} 🔥🔥`);
 });
