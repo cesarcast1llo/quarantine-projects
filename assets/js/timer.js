2ae53154f37580e9891dd0732619602b5fcbcc0e
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-05-14 10:01:37 -0700

    digital clock added, css and push to prod next

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,3 +1,32 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = 'AM';
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  var time = h + ':' + m + ':' + s + ' ' + session;
+  document.getElementById('MyClockDisplay').innerText = time;
+  document.getElementById('MyClockDisplay').textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
 var milliSeconds = 0;
 var seconds = 0;
 var minutes = 0;
@@ -28,7 +57,6 @@ function pause() {
   stopTimer();
 }
 
-//TODO find a quicker way
 function stop() {
   stopTimer();
   minutes = 0;
@@ -50,8 +78,6 @@ function getTimer() {
     `:` +
     (milliSeconds < 10 ? `0` + milliSeconds : milliSeconds)
   );
-  // minutes, seconds, milliseconds have zero in front until they reach 10
-  // not necessary, but for UI
 }
 
 function restart() {
